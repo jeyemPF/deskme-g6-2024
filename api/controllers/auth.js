25f3b3ba907f2a71b6bdeaf136598e6ec64ee7aa
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-03-23 14:48:43 +0800

    updateauth

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,8 +1,8 @@
-export const register = async (req, res, next) =>{
-    try {
-        const newUser = new User
-    }catch(err){
-        next(err)
-    }
+// export const register = async (req, res, next) =>{
+//     try {
+//         const newUser = new User
+//     }catch(err){
+//         next(err)
+//     }
 
-}
+// }
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,8 @@
 import express from "express"
-import { register } from "../controllers/auth"
+// import { register } from "../controllers/auth"
 
 const router = express.Router()
 
-router.get("/register", register )
+// router.get("/register", register )
 
 export default router
