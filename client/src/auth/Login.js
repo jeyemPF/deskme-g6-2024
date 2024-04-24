649fb4c2de8b0fd1b773dd79c1a312c0b8fc1122
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-04-24 20:24:21 +0800

    jc

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -4,8 +4,6 @@ import { BsEye, BsEyeSlash } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios'
 
-<<<<<<< HEAD
-=======
 const InputField = ({ type, name, placeholder, value, onChange, icon }) => {
   return (
     <div className='relative'>
@@ -30,7 +28,6 @@ const InputField = ({ type, name, placeholder, value, onChange, icon }) => {
   );
 };
 
->>>>>>> jc
 function Login() {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
@@ -48,8 +45,6 @@ function Login() {
 
   const navigate = useNavigate();
 
-<<<<<<< HEAD
-=======
   const handleClick1 = () => {
     navigate('/resetpassword');
   };
@@ -58,7 +53,6 @@ function Login() {
     navigate('/');
   };
 
->>>>>>> jc
   const handleLogin = async () => {
     try {
       const credentials = {
@@ -94,69 +88,6 @@ function Login() {
   }, [password, email]);
 
   return (
-<<<<<<< HEAD
-    <div className="flex-col flex justify-center items-center mt-12">
-      <div className="box-border border-2 w-[500px] h-[500px] border-black rounded-xl">
-        <div>
-          <div className="ml-12 pb-16">
-            <button className="ml-[415px] text-2xl pt-3">
-              <HiOutlineXMark />
-            </button>
-            <h1 className="text-4xl font-black">Sign-in</h1>
-            <p>Stay updated on your bookings.</p>
-          </div>
-
-          <div className="text-center">
-            <div className="pb-5">
-              <input
-                value={email}
-                className="border border-black rounded-lg py-3 px-2 w-[80%]"
-                name="email"
-                placeholder="Username or email:"
-                onChange={handleInputChange(setEmail)}
-              />
-            </div>
-
-            <div className="relative">
-              <input
-                className="border border-black rounded-lg py-3 px-2 w-[80%]"
-                type={showPassword ? "text" : "password"}
-                id="password"
-                value={password}
-                onChange={handleInputChange(setPassword)}
-                placeholder="Password:"
-              />
-
-              <button
-                className="absolute top-1/2 transform -translate-y-1/2 right-4 focus:outline-none pr-12"
-                onClick={togglePasswordVisibility}
-              >
-                {showPassword ? <BsEye /> : <BsEyeSlash />}
-              </button>
-            </div>
-          </div>
-
-          <div className="list-none ml-12 pt-2">
-            <li className="font-medium">
-              <a href="#">Forgot password?</a>
-            </li>
-          </div>
-
-          <div className="text-center pt-10 pb-5">
-            <button
-              onClick={handleLogin}
-              className=" text-white bg-black font-semibold rounded-full text-xl py-3 w-[80%] "
-            >
-              Sign in
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="text-center font-light pt-3">
-        <h1>
-          <span>&#169;</span>2023 DeskMe, All right reserved. Privacy Policy{" "}
-          <br /> and Terms & Conditions.
-=======
     <div className='flex flex-col items-center justify-center mt-32'>
       <div className='border-2 border-black rounded-lg shadow-lg w-full max-w-md p-8'>
         <div className='flex justify-end'>
@@ -205,7 +136,6 @@ function Login() {
       <div className='text-center text-base font-light mt-2'>
         <h1>
           <span>&#169;</span>2023 DeskMe, All right reserved. Privacy Policy <br /> and Terms & Conditions.
->>>>>>> jc
         </h1>
       </div>
     </div>
