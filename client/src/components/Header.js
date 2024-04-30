412d8779a105d8d9a9971d9e8a431bd7eeeff7ac
Digs <jcfdiga@gmail.com>
2024-04-30 12:45:33 +0800

    added dropdown for header & added active bg on sidebar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,43 @@
 import React from 'react'
 import { BellIcon } from '@heroicons/react/24/solid';
+import Dropdown from './Dropdown';
 
 const Header = () => {
-
   return (
-    <header className="dark:bg-neutral-900 w-screen bg-white p-4 border-b-[1px] border-black dark:border-neutral-500 dark:shadow-neutral-800">
-    <div className="flex items-center justify-between">
+    <header className="dark:bg-neutral-900 w-screen bg-white p-3 border-b-[1px] border-black dark:border-neutral-500 dark:shadow-neutral-800">
+      <div className="flex items-center justify-between">
         <div className="flex items-center space-x-4">
-        <button className="relative">
-            <BellIcon className="h-8 w-8 text-neutral-700 rounded-full p-1 hover:bg-neutral-700 hover:text-white dark:text-neutral-300 dark:hover:bg-white dark:hover:text-neutral-700" />
-        </button>
-        <button className="focus:outline-none">
-            <img src="https://scontent.fcrk1-3.fna.fbcdn.net/v/t39.30808-6/432775149_3747249338844359_5265507157405488405_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFTzw_9uAbJZ1jiEvCoRKokHlc2p5Q5ZnoeVzanlDlmeg_e-61fWkdDRrXsqzBmzOE7dT5kO24p5m6BHllytThw&_nc_ohc=E-bqFrkZISUQ7kNvgF3vbJr&_nc_ht=scontent.fcrk1-3.fna&oh=00_AfB3IoWBYmAHRjMwlex_UZH8F0E8KCp_Fv2n9Xi9Vd6Upw&oe=6634341D"
-            className="h-9 w-9 rounded-full border-2 border-neutral-700 dark:border-neutral-300 transition duration-300 transform hover:scale-110" />
-        </button>
-        <div className="flex flex-col">
+          <BellIcon className="h-8 w-8 text-neutral-700 rounded-full p-1 hover:bg-neutral-700 hover:text-white dark:text-neutral-300 dark:hover:bg-white dark:hover:text-neutral-700" />
+          <Dropdown>
+            {[
+              <button className="focus:outline-none">
+                <img
+                  src="https://scontent.fcrk1-3.fna.fbcdn.net/v/t39.30808-6/432775149_3747249338844359_5265507157405488405_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFTzw_9uAbJZ1jiEvCoRKokHlc2p5Q5ZnoeVzanlDlmeg_e-61fWkdDRrXsqzBmzOE7dT5kO24p5m6BHllytThw&_nc_ohc=E-bqFrkZISUQ7kNvgF3vbJr&_nc_ht=scontent.fcrk1-3.fna&oh=00_AfB3IoWBYmAHRjMwlex_UZH8F0E8KCp_Fv2n9Xi9Vd6Upw&oe=6634341D"
+                  className="h-9 w-9 rounded-full border-2 border-neutral-700 dark:border-neutral-300 transition duration-300 transform hover:scale-110"
+                />
+              </button>,
+              [
+                'Customize Profile',
+                'Help Support',
+              ].map((item, index) => (
+                <a
+                 key={index}
+                  href="#"
+                  className="block px-2 text-sm text-gray-700 dark:text-neutral-300 hover:bg-gray-100 hover:text-gray-900 dark:hover:text-black"
+                >
+                  {item}
+                </a>
+              )),
+            ]}
+          </Dropdown>
+          <div className="flex flex-col">
             <span className="text-sm font-medium text-gray-800 dark:text-neutral-300 -ml-2">John Carlo</span>
             <span className="text-xs text-gray-500 dark:text-neutral-400 -ml-2">Employee</span>
         </div>
         </div>
-    </div>
+      </div>
     </header>
-  )
-}   
+  );
+};
 
-export default Header
+export default Header;
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -14,6 +14,7 @@ import Header from "../components/Header";
 
 function Dashboard() {
   const [open, setOpen] = useState(true);
+  const [activeMenuItem, setActiveMenuItem] = useState("Dashboard"); // Add a state to track the active menu item
 
   const navigate = useNavigate();
 
@@ -29,7 +30,7 @@ function Dashboard() {
   ];
 
   const Menus = [
-    ...firstMenuItems,
+   ...firstMenuItems,
     { title: "Settings", icon: <MdSettings className="text-2xl" />, gap: true },
     { title: "Logout", icon: <MdLogout className="text-2xl" /> },
   ];
@@ -39,7 +40,7 @@ function Dashboard() {
       <div className="flex">
         <div
           className={`${
-            open ? "w-60" : "w-20"
+            open? "w-60" : "w-20"
           } bg-white dark:bg-neutral-900 h-screen p-5 pt-5 relative duration-500 border-r-[1px] border-black dark:border-neutral-500`}
         >
           <button className="text-black pt-1 ml-2"><Switcher /></button>
@@ -51,16 +52,17 @@ function Dashboard() {
           <PiArrowCircleLeft
             onClick={() => setOpen(!open)}
             className={`text-black bg-white dark:bg-neutral-900 dark:text-neutral-300 absolute cursor-pointer -right-3 top-12 w-6 border-[1px] border-black rounded-full dark:border-neutral-300 ${
-              !open && "rotate-180"
+             !open && "rotate-180"
             } text-4xl`}
           />
           <ul className="pt-2">
             {firstMenuItems.map((Menu, index) => (
               <li
                 key={index}
-                className={`text-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-300 dark:hover:text-black text-sm font-medium flex items-center gap-x-4 p-2 hover:bg-neutral-700 hover:text-white rounded-md cursor-pointer ${
+                className={`text-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-300  dark:hover:text-neutral-900 text-sm font-medium flex items-center gap-x-4 p-2 hover:bg-neutral-700 hover:text-white rounded-md cursor-pointer ${
                   Menu.gap && "mt-9"
-                }`}
+                } ${activeMenuItem === Menu.title? "bg-neutral-700 text-white dark:bg-neutral-300 dark:text-neutral-900" : ""}`}
+                onClick={() => setActiveMenuItem(Menu.title)} // Add an onClick handler to update the active menu item
               >
                 {Menu.icon}
                 <span className={`${!open && "hidden"} origin-left duration-200`}>
@@ -72,13 +74,22 @@ function Dashboard() {
 
           <ul className="flex flex-col lg:mt-60 sm:mt-44 ">
             {/* Render settings and logout items */}
-            <li className="text-neutral-700 text-sm font-medium flex items-center gap-x-4 p-2 hover:bg-neutral-700 hover:text-white rounded-md mt-9 cursor-pointer dark:text-neutral-300 dark:hover:bg-neutral-300 dark:hover:text-black">
+            <li
+              className={`text-neutral-700 text-sm font-medium flex items-center gap-x-4 p-2 hover:bg-neutral-700 hover:text-white rounded-md mt-9 cursor-pointer dark:text-neutral-300 dark:hover:bg-neutral-300 dark:hover:text-neutral-900 ${activeMenuItem === "Settings"? "bg-neutral-700 text-white dark:bg-neutral-300 dark:text-neutral-900" : ""}`}
+              onClick={() => setActiveMenuItem("Settings")}
+            >
               <MdSettings className="text-2xl" />
               <span className={`${!open && "hidden"} origin-left duration-200`}>
                 Settings
               </span>
             </li>
-            <li onClick={handleClick} className="text-neutral-700 text-sm font-medium flex items-center gap-x-4 p-2 mt-1 hover:bg-neutral-700 hover:text-white border-2 border-neutral-700 rounded-md cursor-pointer dark:text-neutral-300 dark:border-neutral-300 dark:hover:bg-neutral-300 dark:hover:text-black">
+            <li
+              onClick={() => {
+                handleClick();
+                setActiveMenuItem("Logout"); // Update the active menu item when logging out
+              }}
+              className={`text-neutral-700 text-sm font-medium flex items-center gap-x-4 p-2 mt-1 hover:bg-neutral-700 hover:text-white border-2 border-neutral-700 rounded-md cursor-pointer dark:text-neutral-300 dark:border-neutral-300 dark:hover:bg-neutral-300 dark:hover:text-black ${activeMenuItem === "Logout"? "bg-neutral-700 text-white" : ""}`}
+            >
               <MdLogout className="text-2xl" />
               <span className={`${!open && "hidden"} origin-left duration-200`}>
                 Logout
