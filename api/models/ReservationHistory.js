214a92781b8289c5981c83f74452a02a61e5aef2
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-05-03 22:52:56 +0800

    update the booking history

--- api/models/ReservationHistory.js
+++ api/models/ReservationHistory.js
@@ -1,37 +1,40 @@
 import mongoose from 'mongoose';
+
 const { Schema } = mongoose;
 
-const ReservationHistorySchema = new Schema(
-    {
-        reservation: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Reservation",
-            required: true,
-        },
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        date: {
-            type: Date,
-            required: true,
-        },
-        type: {
-            type: String,
-            enum: ["REJECTED", "CANCELED", "COMPLETED", "EXPIRED", "ABORTED"],
-            required: true,
-        },
-        startTime: {
-            type: Date,
-            required: true,
-        },
-        endTime: {
-            type: Date,
-            required: true,
-        },
+const ReservationHistorySchema = new Schema({
+    reservation: {
+        type: Schema.Types.ObjectId,
+        ref: 'Reservation',
+        required: true,
     },
-    { timestamps: true }
-);
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    desk: {
+        type: Schema.Types.ObjectId,
+        ref: 'Desk',
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    startTime: {
+        type: Date,
+        required: true,
+    },
+    endTime: {
+        type: Date,
+        required: true,
+    },
+    type: {
+      type: String,
+      enum: ["REJECTED", "CANCELED", "COMPLETED", "EXPIRED", "ABORTED"],
+      required: true,
+    },
+}, { timestamps: true });
 
 export default mongoose.model('ReservationHistory', ReservationHistorySchema);
