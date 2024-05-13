2c1bf0b86e44cb0c072fd5ea15faa515220de7d5
Digs <jcfdiga@gmail.com>
2024-05-13 21:01:14 +0800

    modify favicon, navbar, home

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -45,7 +45,7 @@ const Navbar = () => {
               </li>
             </ul>
 
-            <div className="flex navbar:hidden gap-1">
+            <div className="flex navbar:hidden gap-1 -ml-10">
               <button className="text-neutral-700 text-xl pt-1"><Switcher /></button>
               <button className="text-neutral-700 text-2xl pt-1 dark:text-neutral-300" onClick={toggleMenu}>
                 <IoMenuSharp />
@@ -57,7 +57,7 @@ const Navbar = () => {
               </button>
               <button className="text-neutral-700 text-2xl"><Switcher /></button>
               <p></p>
-              <button onClick={handleClick} className="hidden md:block text-neutral-700 bg-white font-bold rounded-lg border-2 border-neutral-700 shadow-lg text-sm px-6 py-2.5 hover:bg-neutral-700 hover:text-white transition-colors duration-300 dark:text-neutral-300 dark:bg-neutral-900 dark:border-neutral-300 dark:hover:bg-neutral-300 dark:hover:text-neutral-700 dark:transition-colors dark:duration-300">Login</button>
+              <button onClick={handleClick} className="hidden md:block text-neutral-700 bg-white font-bold rounded-lg border-2 border-neutral-700 text-sm px-6 py-2.5 hover:bg-neutral-700 hover:text-white transition-colors duration-300 dark:text-neutral-300 dark:bg-neutral-900 dark:border-neutral-300 dark:hover:bg-neutral-300 dark:hover:text-neutral-700 dark:transition-colors dark:duration-300">Login</button>
             </div>
           </div>
         </div>
