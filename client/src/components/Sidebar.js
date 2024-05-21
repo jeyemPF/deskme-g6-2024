33764a9eb7716b4f1935d42f26f276f0e06b3e46
Digs <jcfdiga@gmail.com>
2024-05-21 23:51:19 +0800

    sidebar one line applied to the tooltip

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -59,7 +59,7 @@ export function SidebarItem({ icon, text, active, alert }) {
             )}
             {!expanded && (
                 <div
-                    className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-gray-100 text-gray-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}
+                    className={`absolute whitespace-nowrap left-full rounded-md px-2 py-1 ml-6 bg-gray-100 text-gray-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}
                 >
                     {text}
                 </div>
