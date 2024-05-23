64b5a00e3caea0c7279a02e9b13265a157d76669
Digs <jcfdiga@gmail.com>
2024-05-23 17:36:52 +0800

    user side added

--- client/src/user/Booking.js
+++ client/src/user/Booking.js
@@ -1,19 +1,54 @@
-import Sidebar from '../components/Sidebar';
+import React, { useState } from 'react';
 import Header from '../components/Header';
-import Deskmap from '../components/Deskmap';
+import Sidebar, { SidebarItem, SidebarProvider, Content } from '../components/Sidebar';
+import { LayoutDashboard, Layers, BookCopy, LifeBuoy, Settings, LogOut, FileCog } from "lucide-react";
+import { useNavigate } from 'react-router-dom';
 
-function Booking() {
+const Booking = () => {
+
+  const navigate = useNavigate();
+
+  const handleSignOutClick = () => {
+    navigate('/login');
+  };
+
+  const handleDashboardClick = () => {
+    navigate('/dashboard');
+  };
+  const handleManageBookingClick = () => {
+    navigate('/managebooking');
+  }
 
   return (
-    <div style={{ height: '100vh', overflowY: 'hidden' }}>
+    <>
       <Header />
-      <div>
-        <Sidebar />
+      <div className="flex dark:bg-neutral-900">
+        <SidebarProvider>
+          <Sidebar>
+            <SidebarItem icon={<LayoutDashboard size={20} onClick={handleDashboardClick} />} text="Dashboard" />
+            <SidebarItem icon={<BookCopy size={20} />} text="Booking" active />
+            <SidebarItem icon={<Layers size={20} onClick={handleManageBookingClick} />} text="Manage Bookings" />
+            <hr className="my-3" />
+            <SidebarItem icon={<Settings size={20} />} text="Settings" />
+            <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
+            <hr className="my-3" />
+            <SidebarItem icon={<LogOut size={20} onClick={handleSignOutClick} />} text="Sign Out" />
+          </Sidebar>
+          <Content>
+            <h1 className='font-bold text-xl mb-3 dark:text-neutral-50'>Bookings</h1>
+            <div className="grid grid-cols-1 gap-4 lg:grid-cols-1 lg:gap-8">
+              <div className="flex flex-row items-center justify-center h-32 rounded-lg bg-gradient-to-r from-orange-50 to-orange-200 border-[1px] border-neutral-100 shadow-sm">
+                <div className='flex flex-col'>
+                  <span className="text-xl font-semibold">Total: 0</span>
+                  <span className="text-sm font-normal">Pending Books</span>
+                </div>
+                <FileCog className="w-10 h-10 ml-10" />
+              </div>
+            </div>
+          </Content>
+        </SidebarProvider>
       </div>
-      <div className='text-center'>
-        <Deskmap/>
-      </div>
-    </div>
+    </>
   );
 };
 
--- client/src/user/ManageBooking.js
+++ client/src/user/ManageBooking.js
@@ -1,187 +1,214 @@
-import React from 'react'
-import Logo from '../assets/Logo.png'
-import { MdOutlineDashboard } from "react-icons/md";
-import { IoBookOutline } from "react-icons/io5";
-import { RiBookLine } from "react-icons/ri";
-import { PiArrowUDownRightLight } from "react-icons/pi";
-import { BiMoon } from "react-icons/bi";
-import { GrNotification } from "react-icons/gr";
-import { LuUserSquare } from "react-icons/lu";
-import { FaRegBookmark } from "react-icons/fa6";
-import { PiDesktopTowerBold } from "react-icons/pi";
-import { PiWarning } from "react-icons/pi";
-import { CgMenuGridO } from "react-icons/cg";
+import React, { useState } from 'react';
+import { LayoutDashboard, Layers, Flag, BookCopy, LifeBuoy, Settings, LogOut, FileCog, ScrollText } from "lucide-react";
 import { useNavigate } from 'react-router-dom';
-import Input from 'postcss/lib/input';
-import Calendar from '../components/Calendar.js';
+import Sidebar, { SidebarItem, SidebarProvider, Content } from '../components/Sidebar'
+import Header from '../components/Header'
 
+const ManageBooking = () => {
+const [isModalOpen, setIsModalOpen] = useState(false);
 
+const handleManageClick = () => {
+    setIsModalOpen(true);
+    };
+const handleCloseModal = () => {
+    setIsModalOpen(false);
+    };
 
+  const navigate = useNavigate();
 
-function ManageBooking() {
+  const handleSignOutClick = () => {
+    navigate('/login');
+  };
 
-    const navigate = useNavigate();
-
-    const handleClick = () => {
+  const handleBookingClick = () => {
     navigate('/booking');
   };
-  const handleDashboard = () => {
+
+  const handleDashboardClick = () => {
     navigate('/dashboard');
   };
 
-  const handleSignout = () => {
-    navigate('/');
-  };
+  const tableItems = [
 
+  ];
 
   return (
-    <div className='bg-gray-100'>
-        <div className='flex flex-row-reverse gap-10'>
-
-            <div className='border-t-2 w-[80%] h-[85px] bg-white rounded-es-2xl'>
-                <div className='flex gap-5 justify-between'>
-                    <h1 className='text-3xl font-black pl-5 pt-8'>Manage Booking</h1>
-                    <div className='flex gap-3 mr-8'>
-                        <div className='flex gap-2 pt-5'> 
-                            <button className="text-black text-xl"><BiMoon /></button>
-                            <p className=' text-xl pt-1'>|</p>
-                            <button className="text-black text-xl"><GrNotification /></button>
-                        </div>
-                        <div className='flex pt-5 gap-3'>
-                            <button className="text-black text-5xl"><LuUserSquare /></button>
-                            <div className='flex-col'>
-                                <h1 className='font-bold'>John Carlo</h1>
-                                <h1 className='text-xs'>Customer</h1>
-                            </div>
-                          
-                        </div>
-                    </div>
+    <>
+      <Header />
+      <div className="flex dark:bg-neutral-900">
+        <SidebarProvider>
+          <Sidebar>
+            <SidebarItem icon={<LayoutDashboard size={20} onClick={handleDashboardClick} />} text="Dashboard" />
+            <SidebarItem icon={<BookCopy size={20} onClick={handleBookingClick} />} text="Booking" />
+            <SidebarItem icon={<Layers size={20} />} text="Manage Bookings" active />
+            <hr className="my-3" />
+            <SidebarItem icon={<Settings size={20} />} text="Settings" />
+            <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
+            <hr className="my-3" />
+            <SidebarItem icon={<LogOut size={20} onClick={handleSignOutClick} />} text="Sign Out" />
+          </Sidebar>
+          <Content>
+            <h1 className='font-bold text-xl mb-3 dark:text-neutral-50'>Manage Bookings</h1>
+            <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
+              <div className="flex flex-row items-center justify-center h-32 rounded-lg bg-gradient-to-r from-green-50 to-green-200 border-[1px] border-neutral-100 shadow-sm">
+                <div className='flex flex-col'>
+                  <span className="text-xl font-semibold">Total: 0</span>
+                  <span className="text-sm font-normal">All Bookings</span>
                 </div>
-                <div className='flex gap-5'>
-                    <div className='pt-10'>
-                        <div className='border w-60 h-32 rounded-xl bg-[linear-gradient(90deg,_#FFFFFF_0%,_#D9F2D4_76.7%)] [box-shadow:0px_4px_4px_0px_#0000001A]'>
-                            <div className='flex justify-center items-center pt-8 gap-5'>
-                                <div>
-                                    <h1 className='text-xl font-bold'>3</h1>
-                                    <p className='font-medium'>All bookings</p>
-                                </div>
-                                <h1 className='text-4xl pt-5'><FaRegBookmark /></h1>
-                            </div>
-                        </div>
-                    </div>
+                <ScrollText className="w-10 h-10 ml-10" />
+              </div>
+              <div className="flex flex-row items-center justify-center h-32 rounded-lg bg-gradient-to-r from-orange-50 to-orange-200 border-[1px] border-neutral-100 shadow-sm">
+                <div className='flex flex-col'>
+                  <span className="text-xl font-semibold">Total: 0</span>
+                  <span className="text-sm font-normal">Pending Books</span>
                 </div>
-
-                <div className='pt-8'>
-                    <div className='border w-[1000px] h-[400px] bg-white rounded-xl [box-shadow:0px_4px_4px_0px_#0000001A]'>
-                      <div className='flex justify-between pt-4'>
-                        <h1 className='text-xl font-bold pl-6'>All bookings | History</h1>
-                        <div className='pr-6'>
-                          <input className='rounded-xl px-3 py-5 border-[1px] border-[solid] border-[#000000] h-10 w-[100%]'
-                            type="text"
-                            placeholder=" Search bookings        🔍"
-                             />
-                          </div>
-                      </div>
-
-                          <div className='border-[1px] rounded-xl ml-6 mt-5 border-[solid] border-[#000000] w-[95%] h-[10%] bg-[#EEEEEE]'>
-                            <div className='flex justify-center gap-[45px] pt-2 text-normal'>
-                              <h1>ID</h1>
-                              <h1>|</h1>
-                              <h1>Name</h1>
-                              <h1>|</h1>
-                              <h1>Booking Number</h1>
-                              <h1>|</h1>
-                              <h1>Check in</h1>
-                              <h1>|</h1>
-                              <h1>Check out</h1>
-                              <h1>|</h1>
-                              <h1>Status</h1>
-                            </div>
-                          </div>
-
-                          <div className='border-[0.5px] border-[solid] border-[#000000] w-[95%] h-[65%] rounded-xl mt-3 ml-6'>
-                            <div className=' rounded-xl ml-5 mt-5 border-none w-[95%] h-[15%] bg-[#EEEEEE]' >
-                              <div className='flex justify-center gap-20 pt-2 pl-3 text-normal'>
-                                  <h1>1</h1>
-                                  <h1>John</h1>
-                                  <h1 className='pl-12'>123456789</h1>
-                                  <h1>March 22, 2024</h1>
-                                  <h1>March 23, 2024</h1>
-                                  <h1>Done</h1>
-                              </div>
-                            </div>
-
-                            <div className=' rounded-xl ml-5 mt-5 border-none w-[95%] h-[15%] bg-[#EEEEEE]' >
-                            <div className='flex justify-center gap-20 pt-2 pl-3 text-normal'>
-                                  <h1>2</h1>
-                                  <h1>John</h1>
-                                  <h1 className='pl-12'>123456789</h1>
-                                  <h1>March 22, 2024</h1>
-                                  <h1>March 23, 2024</h1>
-                                  <h1>Done</h1>
-                              </div>
-                            </div>
-
-                            <div className=' rounded-xl ml-5 mt-5 border-none w-[95%] h-[15%] bg-[#EEEEEE]' >
-                            <div className='flex justify-center gap-20 pt-2 pl-3 text-normal'>
-                                  <h1>3</h1>
-                                  <h1>John</h1>
-                                  <h1 className='pl-12'>123456789</h1>
-                                  <h1>March 22, 2024</h1>
-                                  <h1>March 23, 2024</h1>
-                                  <h1>Done</h1>
-                              </div>
-                            </div>
-                         </div>
-                          
-                      
-                        
-                  
-
-
-                    </div>
-                </div>
-               
-                
+                <FileCog className="w-10 h-10 ml-10" />
+              </div>
             </div>
-           
-            <div className='border-l-2 border-black w-72 h-[700px] bg-white'>
-                <div>
-                    <img className='w-44 pb-10 ml-8' src={Logo}/>
+            <div className="grid grid-cols-1 gap-4 lg:grid-cols mt-6">
+              <div className="rounded-lg bg-white p-5 border-[1px] border-neutral-100 shadow-sm">
+              <div className="flex justify-end items-center">
+                <div className="relative w-60 max-w-md">
+                    <input
+                    type="text"
+                    className="w-full p-2 pr-10 pl-4 rounded-lg border border-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition duration-150 ease-in-out"
+                    placeholder="Search bookings"
+                    />
+                    <div className="absolute right-0 top-0 flex items-center h-full pr-4">
+                    <svg
+                        className="w-5 h-5 text-gray-500"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M21 21l-4.35-4.35M11 19a8 8 0 100-16 8 8 0 000 16z"
+                        />
+                    </svg>
+                    </div>
                 </div>
-
-                <div className='flex font-bold text-xl pb-8 pl-10'>
-                    <button onClick={handleDashboard} className='text-xl flex gap-3'>
-                    <p className='text-4xl'><MdOutlineDashboard /></p>
-                    <h1>Dashboard</h1>
-                </button>
-                 </div>
-
-                <div className='flex font-bold text-xl pb-8 pl-10'>
-                    <button onClick={handleClick} className='text-xl flex gap-3'>
-                    <p className='text-4xl'><RiBookLine /></p>
-                    <h1>Booking</h1>
-                    </button>
                 </div>
-
-                <div className='flex font-bold text-xl pb-8 pl-10'>
-                    <button className='text-xl flex gap-3'>
-                        <p className='text-4xl pt-4'><IoBookOutline /></p>
-                        <h1>Manage<br/>Booking</h1>
-                    </button>
+                <div className="overflow-x-auto">
+                  <table className="w-full table-auto mt-2">
+                    <thead className="text-gray-900 font-medium text-lg border-b text-center">
+                      <tr>
+                        <th className="py-3 pr-6">ID</th>
+                        <th className="py-3 pr-6">Name</th>
+                        <th className="py-3 pr-6">Date</th>
+                        <th className="py-3 pr-6">Time-In</th>
+                        <th className="py-3 pr-6">Time-Out</th>
+                        <th className="py-3 pr-6">Status</th>
+                      </tr>
+                    </thead>
+                    <tbody className="text-gray-600 divide-y text-center text-sm">
+                      {
+                        tableItems.map((item, idx) => (
+                          <tr key={idx}>
+                            <td className="pr-6 py-4 whitespace-nowrap">{item.reservation_id}</td>
+                            <td className="pr-6 py-4 whitespace-nowrap">{item.name}</td>
+                            <td className="pr-6 py-4 whitespace-nowrap">{item.reservation_date}</td>
+                            <td className="pr-6 py-4 whitespace-nowrap">{item.start_time}</td>
+                            <td className="pr-6 py-4 whitespace-nowrap">{item.end_time}</td>
+                            <td className="pr-6 py-4 whitespace-nowrap">
+                              <span className={`px-3 py-2 rounded-full font-semibold text-xs ${item.status === "Active" ? "text-green-600 bg-green-50" : "text-blue-600 bg-blue-50"}`}>
+                                {item.status}
+                              </span>
+                            </td>
+                            <td className="whitespace-nowrap text-center">
+                              <button onClick={handleManageClick} className="py-1.5 px-3 text-gray-600 text-sm hover:text-gray-500 duration-150 hover:bg-gray-50 border rounded-lg">
+                                Manage
+                              </button>
+                            </td>
+                          </tr>
+                        ))
+                      }
+                    </tbody>
+                  </table>
                 </div>
+                <ol className="flex justify-center gap-1 mt-5 text-xs font-medium">
+                  <li>
+                    <a
+                      href="#"
+                      className="inline-flex size-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
+                    >
+                      <span className="sr-only">Prev Page</span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-3 w-3"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </a>
+                  </li>
 
-                <div className='flex font-bold text-xl pb-8 pl-10 '>
-                    <button onClick={handleSignout} className='flex text-xl gap-3 mt-72'>
-                        <p className='text-4xl'><PiArrowUDownRightLight /></p>
-                        <h1>Sign-out</h1>
-                    </button>
-                </div>
+                  <li>
+                    <a
+                      href="#"
+                      className="block size-8 rounded border-blue-600 bg-blue-600 text-center leading-8 text-white"
+                    >
+                      1
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="inline-flex size-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
+                    >
+                      <span className="sr-only">Next Page</span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-3 w-3"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                </ol>
+              </div>
             </div>
+          </Content>
+        </SidebarProvider>
+      </div>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white rounded-lg p-6 w-11/12 max-w-md mx-auto">
+            <h2 className="text-xl font-semibold mb-4">Manage Bookings</h2>
+            <p className="mb-6">Do you want to delete or archive this booking?</p>
+            <div className="flex justify-end space-x-4">
+              <button
+                onClick={handleCloseModal}
+                className="py-2 px-4 bg-gray-500 text-white rounded hover:bg-gray-700 transition duration-200"
+              >
+                Delete
+              </button>
+              <button
+                onClick={handleCloseModal}
+                className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-200"
+              >
+                Archive
+              </button>
+            </div>
+          </div>
         </div>
-        
-
-    </div>
+        )}
+    </>
   )
 }
 
