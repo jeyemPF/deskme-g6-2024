9b2949fe795a73799f21f70990db21e03c61aee4
Digs <jcfdiga@gmail.com>
2024-05-25 19:45:40 +0800

    sidebar fixed all working | sidebar applied to user side

--- client/src/user/Dashboard.js
+++ client/src/user/Dashboard.js
@@ -53,14 +53,14 @@ const Dashboard = () => {
       <div className="flex dark:bg-neutral-900">
       <SidebarProvider>
         <Sidebar>
-          <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" active />
-          <SidebarItem icon={<BookCopy size={20} onClick={handleBookingClick} />} text="Booking" />
-          <SidebarItem icon={<Layers size={20} onClick={handleManageBookingClick} />} text="Manage Bookings" />
-          <hr className="my-3" />
-          <SidebarItem icon={<Settings size={20} />} text="Settings" />
-          <SidebarItem icon={<LifeBuoy size={20} />} text="Help"/>
-          <hr className="my-3" />
-          <SidebarItem icon={<LogOut size={20} onClick={handleSignOutClick} />} text="Sign Out"/>
+            <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" active />
+            <SidebarItem icon={<BookCopy size={20} />} text="Booking" onClick={handleBookingClick} />
+            <SidebarItem icon={<Layers size={20} />} text="Manage Bookings" onClick={handleManageBookingClick} />
+            <hr className="my-3" />
+            <SidebarItem icon={<Settings size={20} />} text="Settings" />
+            <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
+            <hr className="my-3" />
+            <SidebarItem icon={<LogOut size={20} />} text="Sign Out" onClick={handleSignOutClick} />
         </Sidebar>
         <Content>
                 <h1 className='font-bold text-xl mb-3 dark:text-neutral-50'>Dashboard</h1>
