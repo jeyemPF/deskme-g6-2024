4fab3509799b8779f8cb71427402f552bdc3b62f
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-05-31 22:28:28 +0800

    update index.js

--- api/index.js
+++ api/index.js
@@ -16,13 +16,14 @@ import switchsRoute from './routes/switchs.js';
 import otpRoutes from './routes/otpRoutes.js';
 import auditTrailsRoute from './routes/auditTrails.js';
 
+
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', "https://deskme-g6-2024.vercel.app"],
+    origin: ["https://deskme-g6-2024.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   },
 });
@@ -34,7 +35,7 @@ cloudinary.config({
 });
 
 // Configure CORS
-const allowedOrigins = ['http://localhost:3000', "https://deskme-g6-2024.vercel.app"];
+const allowedOrigins = ["https://deskme-g6-2024.vercel.app"];
 app.use(cors({
   origin: function (origin, callback) {
     if (allowedOrigins.includes(origin) || !origin) {
@@ -105,8 +106,4 @@ io.on('connection', (socket) => {
   });
 });
 
-export default function () {
-  return server;
-}
-
-export { io };
+export { server, io };
