91d48f11821b752aba5a85e3f193a2ee2d84b07e
Johnmack Faeldonia <147502433+jeyemPF@users.noreply.github.com>
2024-06-01 06:19:18 +0800

    Update login page 3

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -1,110 +1,179 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import cloudinary from './config/cloudinary.js';
+import React, { useEffect, useState } from "react";
+import { HiOutlineXMark } from "react-icons/hi2";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
-// Import routes
-import authRoute from './routes/auth.js';
-import usersRoute from './routes/users.js';
-import desksRoute from './routes/desks.js';
-import reservationsRoute from './routes/reservations.js';
-import switchsRoute from './routes/switchs.js';
-import otpRoutes from './routes/otpRoutes.js';
-import auditTrailsRoute from './routes/auditTrails.js';
-
-dotenv.config();
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "https://deskme-g6-2024.vercel.app",
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true,
-  },
-});
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-// Configure CORS
-const allowedOrigins = ["https://deskme-g6-2024.vercel.app"];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  credentials: true,
-}));
-
-// Connect to MongoDB
-const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO);
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    throw error;
-  }
+const InputField = ({ type, name, placeholder, value, onChange, icon, error }) => {
+  return (
+    <div className='relative'>
+      <input
+        className='border border-black rounded-md py-3 px-3 w-full'
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+      {icon && (
+        <button
+          type="button"
+          className='absolute top-1/2 transform -translate-y-1/2 right-1 focus:outline-none pr-4'
+          onClick={icon.onClick}
+        >
+          {icon.component}
+        </button>
+      )}
+      {error && <p className='text-red-500 text-sm'>{error}</p>}
+    </div>
+  );
 };
 
-mongoose.connection.on('disconnected', () => {
-  console.log('MongoDB disconnected');
-});
+function Login() {
+  // Email
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState('');
+  // Password
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  // Toggle Password Visibility
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-mongoose.connection.on('connected', () => {
-  console.log('MongoDB connected');
-});
+  // Navigation
+  const navigate = useNavigate();
 
-// Middlewares
-app.use(cookieParser());
-app.use(express.json());
+  const handleClick1 = () => {
+    navigate('/resetpassword');
+  };
 
-// Use routes
-app.use('/api/auth', authRoute);
-app.use('/api/users', usersRoute);
-app.use('/api/desks', desksRoute);
-app.use('/api/reservations', reservationsRoute);
-app.use('/api/switchs', switchsRoute);
-app.use('/api/auditTrails', auditTrailsRoute);
-app.use('/api/otp', otpRoutes);
+  const handleClick2 = () => {
+    navigate('/');
+  };
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || 'Something went wrong';
-  return res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: err.stack,
-  });
-});
+  // Validations
+  const isEmpty = (str) => {
+    return str.trim() === '';
+  };
 
-// Start the server
-const port = process.env.PORT || 8800;
-server.listen(port, () => {
-  connect();
-  console.log(`Connected to backend! Server running on port ${port}`);
-});
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setEmailError('');
+    setPasswordError('');
 
-// Socket.io connection handler
-io.on('connection', (socket) => {
-  console.log('A user connected');
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (isEmpty(email)) {
+      setEmailError('Please fill out this field.');
+      return;
+    } else if (!emailRegex.test(email)) {
+      setEmailError('Please enter a valid email address.');
+      return;
+    }
+    if (isEmpty(password)) {
+      setPasswordError('Please fill out this field.');
+      return;
+    }
 
-// Export the app as the default export
-export default app;
+    try {
+      const credentials = { email, password };
+      const response = await axios.post('https://deskme-g6-2024-server.vercel.app/api/auth/login', credentials);
+
+      console.log('Login successful');
+      console.log('Response:', response.data);
+      
+      // Store the token in localStorage
+      localStorage.setItem('token', response.data.token);
+
+      // Ideally, the token should be stored in a more secure manner (e.g., HTTP-only cookies)
+      sessionStorage.setItem('userCredentials', JSON.stringify(response.data));
+
+      // Redirect based on user role
+      const userRole = response.data.user.role;
+      if (userRole === 'admin') {
+        navigate("/admindashboard");
+      } else if (userRole === 'superadmin') {
+        navigate("/superdashboard");
+      } else if (userRole === 'officemanager') {
+        navigate('/officedashboard');
+      } else {
+        navigate("/dashboard");
+      }
+
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        console.error('Incorrect password');
+        setPasswordError('Incorrect password'); // Set error message for incorrect password
+      } else {
+        console.error('Login failed:', error.response ? error.response.data.message : error.message);
+        setPasswordError('Login failed. Please try again.');
+      }
+    }
+  };
+
+  useEffect(() => {
+    console.log(email);
+    console.log(password);
+  }, [password, email]);
+
+  return (
+    <div className='flex flex-col items-center justify-center mt-32'>
+      <div className='border-[1px] border-neutral-700 rounded-lg shadow-lg w-full max-w-md p-8'>
+        <div className='flex justify-end'>
+          <button onClick={handleClick2} className='text-2xl text-neutral-700'>
+            <HiOutlineXMark />
+          </button>
+        </div>
+        <h1 className='text-4xl font-black text-left text-neutral-700'>Sign in</h1>
+        <p className='font-normal text-left mt-1 text-neutral-700'>Stay updated on your bookings.</p>
+
+        <form onSubmit={handleLogin} className='mt-12'>
+          <InputField
+            type='email'
+            name='email'
+            placeholder='Email Address:'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            icon={null}
+            error={emailError}
+          />
+          <div className='relative mt-4'>
+            <InputField
+              type={showPassword ? 'text' : 'password'}
+              name='password'
+              placeholder='Password:'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              icon={{
+                component: showPassword ? <BsEye /> : <BsEyeSlash />,
+                onClick: togglePasswordVisibility,
+              }}
+              error={passwordError}
+            />
+          </div>
+
+          <div className="flex items-left mt-4 mb-4">
+            <input type="checkbox" id="rememberMe" className="w-4 h-5 text-neutral-700 bg-gray-100 border-black rounded hover:cursor-pointer" />
+            <label htmlFor="rememberMe" className="ms-2 text-sm font-medium text-neutral-700 hover:cursor-pointer">Remember Me</label>
+            <div className="list-none ml-auto">
+              <li onClick={handleClick1} className="font-medium text-sm hover:underline cursor-pointer text-neutral-700">Forgot password?</li>
+            </div>
+          </div>
+
+          <div className='text-center mt-14 mb-5'>
+            <button type="submit" className='bg-white text-neutral-700 font-semibold rounded-2xl border-[1px] border-neutral-700 py-3 w-full hover:bg-neutral-700 hover:text-white transition-colors duration-300'>Sign in</button>
+          </div>
+        </form>
+      </div>
+      <div className='text-center text-base font-light mt-2 text-neutral-700'>
+        <h1>
+          <span>&#169;</span>2023 DeskMe, All right reserved. Privacy Policy <br /> and Terms & Conditions.
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
