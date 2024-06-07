ebfaac8d8e31df73c8138e5362ee30358d76d912
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-06-08 06:49:33 +0800

    update the updateProfile in backend

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -281,15 +281,6 @@ export const updateProfile = async (req, res) => {
       });
     }
 
-    // Validate the new username
-    const usernameRegex = /^[a-zA-Z_]+$/;
-    if (username && !usernameRegex.test(username)) {
-      return res.status(400).json({
-        success: false,
-        error: "Invalid username",
-      });
-    }
-
     // Update the username if provided
     if (username) {
       user.username = username;
