197d9524fb2b2054588fa88d32d8670ad35d8f15
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-10-24 20:29:15 +0800

    test: test login

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -78,6 +78,7 @@ const Login = () => {
   
     setLoading(true);
     try {
+      // test login
       const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, { email, password });
       localStorage.setItem('token', response.data.token);
       localStorage.setItem('userId', response.data.user._id); // Store the user ID
