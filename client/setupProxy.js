b8be8e47cf7121cebe309ddbd44475f09eefd042
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-10-24 17:11:52 +0800

    Implement Route guards and setup a proxy to run the App again

---
+++ client/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8800', // Backend server
+      changeOrigin: true,
+    })
+  );
+};
--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import Home from '../src/pages/Home';
-import About from '../src/pages/About';
-import Services from '../src/pages/Services';
-import Contact from '../src/pages/Contact';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Authenticate from '../src/auth/Authenticate';
-import Login from '../src/auth/Login'
-import AuthenticateSuccess from '../src/auth/AuthenticateSuccess';
-import Dashboard from '../src/user/Dashboard';
-import Booking from '../src/user/Booking';
-import ManageBooking from '../src/user/ManageBooking';
+// src/App.js
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Authenticate from './auth/Authenticate';
+import Login from './auth/Login';
+import AuthenticateSuccess from './auth/AuthenticateSuccess';
+import Dashboard from './user/Dashboard';
+import Booking from './user/Booking';
+import ManageBooking from './user/ManageBooking';
 import ResetPassword from './auth/ResetPassword';
 import NewPassword from './auth/NewPassword';
 import ADashboard from './admin/ADashboard';
@@ -23,55 +24,194 @@ import SAReports from './superadmin/SAReports';
 import SAAuditReport from './superadmin/SAAuditReport';
 import OMDashboard from './officemanager/OMDashboard';
 import OMBooking from './officemanager/OMBooking';
-import OMReports from './officemanager/OMReports'
+import OMReports from './officemanager/OMReports';
 import OMManageBooking from './officemanager/OMManageBooking';
-import MyBooking from '../src/user/ManageBooking';
+import MyBooking from './user/ManageBooking';
 import Error from './components/Error';
-import Help from './user/Help'
-import Manual from './components/Manual'
-
+import Help from './user/Help';
+import Manual from './components/Manual';
+import ProtectedRoute from './auth/ProtectedRoute';
 
 function App() {
   return (
-  <div>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/authenticate" element={<Authenticate />} />
-        <Route path="/authenticatesuccess" element={<AuthenticateSuccess />} />
-        <Route path='/dashboard' element={<Dashboard />} /> 
-        <Route path='/booking' element={<Booking />} />
-        <Route path='/mybooking' element={<MyBooking />} />
-        <Route path='/resetpassword' element={<ResetPassword />} />
-        <Route path='/newpassword/:token/:id' element={<NewPassword />} />
-        <Route path='/admindashboard' element={<ADashboard />} />
-        <Route path='/adminbooking' element={<ABooking />} />
-        <Route path='/adminmanagebooking' element={<AManageBooking />} />
-        <Route path= '/adminmanage' element={<AManage/>} />
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/authenticate" element={<Authenticate />} />
+          <Route path="/authenticatesuccess" element={<AuthenticateSuccess />} />
+          <Route path='/resetpassword' element={<ResetPassword />} />
+          <Route path='/newpassword/:token/:id' element={<NewPassword />} />
 
-        <Route path='/superdashboard' element={<SADashboard />} />
-        <Route path='/superbooking' element={<SABooking />} />
-        <Route path='/supermanagebooking' element={<SAManageBooking />} />
-        <Route path='/superprivmanage' element={<SAPManage />} />
-        <Route path='/superreports' element={<SAReports />} />
-        <Route path='/superaudit' element={<SAAuditReport />} />
-        <Route path='/officedashboard' element={<OMDashboard />} />
-        <Route path='/officebooking' element={<OMBooking />} />
-        <Route path='/officemanagebooking' element={<OMManageBooking />} />
-        <Route path='/officereports' element={<OMReports />} />
-        <Route path='/error' element={<Error />} />
-        <Route path='/help' element={<Help />} />
-        <Route path='/manual' element={<Manual />} />
+          {/* Protected Routes for User  */}
+          <Route 
+            path="/dashboard"
+            element={
+              <ProtectedRoute allowedRoles={['user']}>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+            />
 
-        
-      </Routes>
-    </Router> 
-  </div>
-  
+          <Route 
+            path="/booking"
+            element={
+              <ProtectedRoute allowedRoles={['user']}>
+                <Booking />
+              </ProtectedRoute>
+            }
+            />
+
+          <Route 
+            path="/mybooking"
+            element={
+              <ProtectedRoute allowedRoles={['user']}>
+                <MyBooking />
+              </ProtectedRoute>
+            }
+            />
+
+          <Route 
+            path="/help" 
+            element={
+              <ProtectedRoute allowedRoles={['user']}>
+                <Help />
+              </ProtectedRoute>
+            } />
+
+            
+
+
+          {/* Protected Routes for Admin */}
+          <Route 
+            path="/admindashboard" 
+            element={
+              <ProtectedRoute allowedRoles={['admin']}>
+                <ADashboard />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/adminbooking" 
+            element={
+              <ProtectedRoute allowedRoles={['admin']}>
+                <ABooking />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/adminmanagebooking" 
+            element={
+              <ProtectedRoute allowedRoles={['admin']}>
+                <AManageBooking />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/adminmanage" 
+            element={
+              <ProtectedRoute allowedRoles={['admin']}>
+                <AManage />
+              </ProtectedRoute>
+            } 
+          />
+
+          {/* Protected Routes for Super Admin */}
+          <Route 
+            path="/superdashboard" 
+            element={
+              <ProtectedRoute allowedRoles={['superadmin']}>
+                <SADashboard />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/superbooking" 
+            element={
+              <ProtectedRoute allowedRoles={['superadmin']}>
+                <SABooking />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/supermanagebooking" 
+            element={
+              <ProtectedRoute allowedRoles={['superadmin']}>
+                <SAManageBooking />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/superprivmanage" 
+            element={
+              <ProtectedRoute allowedRoles={['superadmin']}>
+                <SAPManage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/superreports" 
+            element={
+              <ProtectedRoute allowedRoles={['superadmin']}>
+                <SAReports />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/superaudit" 
+            element={
+              <ProtectedRoute allowedRoles={['superadmin']}>
+                <SAAuditReport />
+              </ProtectedRoute>
+            } 
+          />
+
+          {/* Protected Routes for Office Manager */}
+          <Route 
+            path="/officedashboard" 
+            element={
+              <ProtectedRoute allowedRoles={['officemanager']}>
+                <OMDashboard />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/officebooking" 
+            element={
+              <ProtectedRoute allowedRoles={['officemanager']}>
+                <OMBooking />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/officemanagebooking" 
+            element={
+              <ProtectedRoute allowedRoles={['officemanager']}>
+                <OMManageBooking />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/officereports" 
+            element={
+              <ProtectedRoute allowedRoles={['officemanager']}>
+                <OMReports />
+              </ProtectedRoute>
+            } 
+          />
+
+
+          <Route path="*" element={<Error />} />
+
+          <Route path="/error" element={<Error />} />
+          <Route path="/manual" element={<Manual />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
